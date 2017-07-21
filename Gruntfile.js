a747c229d3ec3030633bebf3021226053458f377
Mike Henderyckx <mike.henderyckx@gmail.com>
2017-07-21 11:21:10 +0200

    Added grunt-sass + dynamic naming of grunt outputfiles from package.json with <%= pkg.name =>

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 
     var paths = {
         src: {
+            css: 'src/css/scss/**/*.scss',
             js: [
                 'src/js/module.js',
                 'src/js/*.js', // files: config.js, run.js, info.js
@@ -20,6 +21,8 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
 
+        pkg: grunt.file.readJSON('package.json'),
+
         uglify: {
             production: {
                 options: {
@@ -27,21 +30,26 @@ module.exports = function(grunt) {
                     sourceMap: true
                 },
                 files: {
-                    'dist/ng1-skeleton-grunt.min.js': paths.src.js
+                    'dist/<%= pkg.name %>.min.js': paths.src.js
                 }
             }
         },
 
         concat: {
-            dist: {
+            development: {
                 src: paths.src.js,
-                dest: 'dist/ng1-skeleton-grunt.js'
+                dest: 'dist/<%= pkg.name %>.js'
             }
         },
 
         clean: {
-            dist: 'dist/*',
-            development: 'dist/ng1-skeleton-grunt.js'
+            developmentJS: {
+                src: ['dist/<%= pkg.name %>.js']
+            },
+            developmentCSS: {
+                src: ['dist/<%= pkg.name %>.css']
+            },
+            all: 'dist/*'
         },
 
         watch: {
@@ -51,29 +59,57 @@ module.exports = function(grunt) {
             js: {
                 files: paths.src.js,
                 tasks: [
+                    'clean:developmentJS',
                     'concat',
                     'eslint'
+                ]
+            },
+            css: {
+                files: paths.src.css,
+                tasks: [
+                    'clean:developmentCSS',
+                    'sass:development'
                 ]
             }
         },
 
         eslint: {
             target: paths.src.js
+        },
+
+        sass: {
+            development: {
+                files: {
+                    'dist/<%= pkg.name %>.css': 'src/css/scss/base.scss'
+                }
+            },
+            production: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    'dist/<%= pkg.name %>.min.css': 'src/css/scss/base.scss'
+                }
+            }
         }
 
     });
 
     // DEVELOPMENT
     grunt.registerTask('development', [
-        'clean:development',
-        'concat'
+        'clean:developmentJS',
+        'clean:developmentCSS',
+        'concat:development',
+        'sass:development'
     ]);
 
     // PRODUCTION
     grunt.registerTask('production', [
-        'clean:dist',
+        'clean:all',
         'concat',
-        'uglify:production'
+        'sass',
+        'uglify'
     ]);
 
     // DEFAULT
