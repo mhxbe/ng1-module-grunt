2bdeb72bcf67cf92426b4230eba1daa7fc84fdef
Mike Henderyckx <mike.henderyckx@gmail.com>
2017-07-22 02:13:48 +0200

    Added comment on info.js file + compiled dist folder

--- dist/ng1-module-grunt.js
+++ dist/ng1-module-grunt.js
@@ -16,7 +16,8 @@ angular
 angular
     .module('ng1-module-grunt')
     .info({
-        version: '0.0.0'
+        version: '0.0.0', // Don't modify version manually! grunt bump will modify this automagically
+        hello: 'hello'
     });
 
 'use strict';
--- src/js/info.js
+++ src/js/info.js
@@ -2,5 +2,6 @@
 angular
     .module('ng1-module-grunt')
     .info({
-        version: '0.0.0'
+        version: '0.0.0', // Don't modify version manually! grunt bump will modify this automagically
+        hello: 'hello'
     });
